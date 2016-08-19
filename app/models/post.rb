f6a8fa68d809f6069754e98e09b5bf97949fa3f4
dowdje <dowdje89@gmail.com>
2016-08-19 16:35:04 -0400

    Validate, Invalidated, whatever

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250}
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-fiction)}
+  validate :not_clickbait
+
+  def not_clickbait
+    unless title.present? && title.match(/Won't\sBelieve|Secret|Top \d/)
+      errors.add(:title)
+    end
+  end
 end
